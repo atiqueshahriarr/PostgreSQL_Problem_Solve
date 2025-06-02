dd13c5573b53817b38106f52653d67905b9f7892
atique-shahriar <satique06@gmail.com>
2025-06-02 10:09:45 +0600

    Create least 2 tables

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -3,3 +3,22 @@ CREATE TABLE rangers (
     name VARCHAR(50),
     region VARCHAR(100)
 );
+
+
+
+CREATE TABLE species (
+    species_id SERIAL PRIMARY KEY,
+    common_name VARCHAR(100),
+    scientific_name VARCHAR(100),
+    discovery_date DATE,
+    conservation_status VARCHAR(50)
+);
+
+CREATE TABLE sightings (
+    sighting_id SERIAL PRIMARY KEY,
+    species_id INT REFERENCES species(species_id),
+    ranger_id INT REFERENCES rangers(ranger_id),
+    location VARCHAR(70),
+    sighting_time TIMESTAMP,
+    notes VARCHAR(100)
+);
