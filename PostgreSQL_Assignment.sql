e914fecdcc17b6e8ecd4d9b0e3c2aedcba2da066
atique-shahriar <satique06@gmail.com>
2025-06-02 10:48:58 +0600

    Insert data

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -4,21 +4,111 @@ CREATE TABLE rangers (
     region VARCHAR(100)
 );
 
-
-
 CREATE TABLE species (
     species_id SERIAL PRIMARY KEY,
-    common_name VARCHAR(100),
+    common_name VARCHAR(50),
     scientific_name VARCHAR(100),
     discovery_date DATE,
-    conservation_status VARCHAR(50)
+    conservation_status VARCHAR(30)
 );
 
 CREATE TABLE sightings (
     sighting_id SERIAL PRIMARY KEY,
-    species_id INT REFERENCES species(species_id),
     ranger_id INT REFERENCES rangers(ranger_id),
-    location VARCHAR(70),
+    species_id INT REFERENCES species(species_id),
     sighting_time TIMESTAMP,
-    notes VARCHAR(100)
+    location VARCHAR(100),
+    notes VARCHAR(200)
 );
+
+
+INSERT INTO rangers (name, region)
+VALUES
+  ('Alice Green', 'Northern Hills'),
+  ('Bob White', 'River Delta'),
+  ('Carol King', 'Mountain Range');
+
+
+INSERT INTO species (common_name, scientific_name, discovery_date, conservation_status)
+VALUES
+  ('Snow Leopard', 'Panthera uncia', '1775-01-01', 'Endangered'),
+  ('Bengal Tiger', 'Panthera tigris', '1758-01-01', 'Endangered'),
+  ('Red Panda', 'Ailurus fulgens', '1825-01-01', 'Vulnerable'),
+  ('Asiatic Elephant', 'Elephas maximus', '1758-01-01', 'Endangered');
+
+
+INSERT INTO sightings (species_id, ranger_id, location, sighting_time, notes) VALUES
+(1, 1, 'Peak Ridge', '2024-05-10 07:45:00', 'Camera trap image captured'),
+(2, 2, 'Bankwood Area', '2024-05-12 16:20:00', 'Juvenile seen'),
+(3, 3, 'Bamboo Grove East', '2024-05-15 09:10:00', 'Feeding observed'),
+(1, 2, 'Snowfall Pass', '2024-05-18 18:30:00', NULL);
+
+SELECT * FROM rangers;
+SELECT * FROM species;
+SELECT * FROM sightings;
+
+
+
+
+
+--1
+
+
+
+
+
+--2
+
+
+
+
+
+
+--3
+
+
+
+
+
+--4
+
+
+
+
+
+--5
+
+
+
+
+
+--6
+
+
+
+
+
+
+--7
+
+
+
+
+
+
+--8
+
+
+
+
+
+
+--9
+
+
+
+
+
+
+
+--10
